a84de94a0cbf37040d80827b1813f69421b75035
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-02 07:30:02 -0400

    Autocommit por Git-Cron: alterações: add 'home/bin/ping.sh' add 'mpd/.mpd/mpd.db' add 'mpd/.mpd/mpd.pid'

--- home/bin/ping.sh
+++ home/bin/ping.sh
@@ -12,23 +12,24 @@ offline="phone-outgoing-busy"
 
 h="8.8.8.8"
 r=1
-v=1
 l=0
 
 function pingar {
 	ping -q -c$r $h > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]; then
-		if [ $v == 1 ]; then
-			#echo -e "Conexão: \033[0;32mOK\033[0m"
+		if [ "$1" == "-v" ]; then
 			echo -e "\033[0;32monline\033[0m"
 		fi
-		export DISPLAY=:0 ; canberra-gtk-play -i $online 2>&1
+		if [ ! -f /tmp/online.lock ]; then
+			export DISPLAY=:0 ; canberra-gtk-play -i $online 2>&1
+			touch /tmp/online.lock
+		fi
 	else
-		if [ $v == 1 ]; then
-			#echo -e "Conexão: \033[0;31mFALHOU\033[0m"
+		if [ "$1" == "-v" ]; then
 			echo -e "\033[0;31moffline\033[0m"
 		fi
 		export DISPLAY=:0 ; canberra-gtk-play -i $offline 2>&1
+		[ -f /tmp/online.lock ] && rm /tmp/online.lock
 	fi
 }
 
