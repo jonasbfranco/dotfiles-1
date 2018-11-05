386c456da622a3a48d2e0ab36d761184a44183f2
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-05 09:15:01 -0300

    Autocommit por Git-Cron: alterações: add 'home/bin/ntfsmount.sh' add 'i3/.config/i3/config' add 'mpd/.mpd/mpd.state'

--- home/bin/ntfsmount.sh
+++ home/bin/ntfsmount.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
+function contem() {
+    local n=$#
+    local valor=${!n}
+    for ((i=1;i < $#;i++)) {
+        if [ "${!i}" == "${valor}" ]; then
+            echo "s"
+            return 0
+        fi
+    }
+    echo "n"
+    return 1
+}
+
 excl=("/dev/sda1")
 
 for d in /dev/sda*; do
 	tipo=$(udisksctl info -b $d | grep IdType | awk '{print $2}')
 	montagem=$(udisksctl info -b $d | grep MountPoints | awk '{print $2}')
-	if [ "$tipo" == "ntfs" ] && [ "$montagem" != "$excl" ]; then
+	if [ "$tipo" == "ntfs" ] && [ $(contem "${excl[@]}" $d) == "n" ] && [ "$montagem" == "" ]; then
 		udisksctl mount -b $d
+		montagem=$(udisksctl info -b $d | grep MountPoints | awk '{print $2}')
 		notify-send "AutoMount" "$d montado em $montagem"
 	fi
 done
