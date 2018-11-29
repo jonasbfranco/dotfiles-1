9df353a094ec044785ed0eb8dea1ed8873b6cea0
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-29 16:30:01 -0300

    Autocommit por Git-Cron: alterações: add 'home/bin/comment.sh' add 'mpd/.mpd/mpd.state'

--- home/bin/comment.sh
+++ home/bin/comment.sh
@@ -3,12 +3,12 @@
 string="$1"
 
 case $2 in
-comentar)
-	sed -e "/$string/ s/^#*/#/" -i $3
-	#sed -i "s/^#\($string\)\$/\1/" $3
+	comentar)
+		sed -e "/$string/ s/^#*/#/" -i $3
+		#sed -i "s/^#\($string\)\$/\1/" $3
     ;;
-descomentar)
-	sed -e "/$string/ s/^#*//" -i $3
-    #sed -i "s/^$string\$/#&/" $3
+	descomentar)
+		sed -e "/$string/ s/^#*//" -i $3
+    	#sed -i "s/^$string\$/#&/" $3
     ;;
 esac
