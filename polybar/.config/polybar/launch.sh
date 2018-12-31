6f24cf65f96346568dff924246d77bfda41a50d8
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-12-31 16:55:01 -0300

    Autocommit por Git-Cron: alterações: add 'polybar/.config/polybar/launch.sh'

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 # B & W
-conf="${HOME}/.config/polybar/config"
-if grep -q "Tema Branco" "$conf"; then
-    cat ${HOME}/.config/polybar/preto > ${HOME}/.config/polybar/config
-else
-	cat ${HOME}/.config/polybar/branco > ${HOME}/.config/polybar/config
-fi
-cat ${HOME}/.config/polybar/principal >> ${HOME}/.config/polybar/config
+#conf="${HOME}/.config/polybar/config"
+#if grep -q "Tema Branco" "$conf"; then
+#    cat ${HOME}/.config/polybar/preto > ${HOME}/.config/polybar/config
+#else
+#	cat ${HOME}/.config/polybar/branco > ${HOME}/.config/polybar/config
+#fi
+#cat ${HOME}/.config/polybar/principal >> ${HOME}/.config/polybar/config
 
 
 # Terminate already running bar instances
