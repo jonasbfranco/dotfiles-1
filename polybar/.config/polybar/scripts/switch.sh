4db9fb909b112f7e0fb61c6c629b6ce5340b7f28
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-12-31 15:15:01 -0300

    Autocommit por Git-Cron: alterações: add 'polybar/.config/polybar/scripts/switch.sh'

--- polybar/.config/polybar/scripts/switch.sh
+++ polybar/.config/polybar/scripts/switch.sh
@@ -1,44 +1,23 @@
 #!/usr/bin/env bash
 
-#  
-
-COR=$(awk -F# '/alert/{print $2;exit}' ${HOME}/.config/polybar/config)
-TRASH_DIRECTORY="${HOME}/.local/share/Trash"
-TRASH_TEMP="/tmp/lixo"
-
-if [[ "${TRASH_DIRECTORY}" = "" ]]; then
-  TRASH_DIRECTORY=${XDG_DATA_HOME:-"${HOME}/.local/share/Trash"}
-fi
-
-if [[ "${1}" == "-x" ]]; then
-	if [ ! -d $TRASH_TEMP ]; then
-		mkdir $TRASH_TEMP
-	fi
-
-	cp -rf ${TRASH_DIRECTORY}/files ${TRASH_TEMP}/
-	cp -rf ${TRASH_DIRECTORY}/info ${TRASH_TEMP}/
-
-	rm -rf ${TRASH_DIRECTORY}/files
-	rm -rf ${TRASH_DIRECTORY}/info
-
-	mkdir ${TRASH_DIRECTORY}/files
-	mkdir ${TRASH_DIRECTORY}/info
-
-	if xset q &>/dev/null; then
-		# ls /usr/share/sounds/freedesktop/stereo/
-		export DISPLAY=:0 ; canberra-gtk-play -i trash-empty 2>&1
-		export DISPLAY=:0 ; notify-send "Lixeira" "Lixeira limpa!"
-	fi
-elif [[ "${1}" == "-o" ]]; then
-	xdg-open $TRASH_DIRECTORY/files
-fi
-
-TRASH_COUNT=$(ls -U -1 "${TRASH_DIRECTORY}/files" | wc -l)
-
-if [[ ${TRASH_COUNT} -gt 0 ]]; then
-	s="%{F#${COR}}%{F-} ${TRASH_COUNT}"
+if grep -Fxq "Tema Branco" ${HOME}/.config/polybar/config
+then
+    switch=""
+    if [ $1 ]; then
+    	cat ${HOME}/.config/polybar/preto > ${HOME}/.config/polybar/config
+    fi
 else
-	s=""
+	COR=$(awk -F# '/alert/{print $2;exit}' ${HOME}/.config/polybar/config)
+	switch="%{F#${COR}}%{F-}"
+    if [ $1 ]; then
+    	cat ${HOME}/.config/polybar/branco > ${HOME}/.config/polybar/config
+	fi
 fi
 
-echo "${s}"
+if [ $1 ]; then
+	cat ${HOME}/.config/polybar/principal >> ${HOME}/.config/polybar/config
+	~/.config/polybar/launch.sh
+	export DISPLAY=:0 ; canberra-gtk-play -i trash-empty 2>&1
+fi
+
+echo "${switch}"
