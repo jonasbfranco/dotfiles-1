4e754e20203f5f9fc3520cd28e75e03aabb997e1
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-12-31 16:25:01 -0300

    Autocommit por Git-Cron: alterações: add 'polybar/.config/polybar/branco' add 'polybar/.config/polybar/config' add 'polybar/.config/polybar/preto' add 'polybar/.config/polybar/scripts/switch.sh' add 'polybar/.config/polybar/scripts/wallpaper.sh'

--- polybar/.config/polybar/scripts/switch.sh
+++ polybar/.config/polybar/scripts/switch.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-grep -Fx "Tema Preto" ${HOME}/.config/polybar/config
+conf="${HOME}/.config/polybar/config"
 
-if [ $? = 1 ]; then 
+if grep -q "Tema Branco" "$conf"; then
     SW=""
     if [ $1 ]; then
     	cat ${HOME}/.config/polybar/preto > ${HOME}/.config/polybar/config
     fi
 else
-	COR=$(awk -F# '/alert/{print $2;exit}' ${HOME}/.config/polybar/config)
-	SW="%{F#${COR}}%{F-}"
+	#COR=$(awk -F# '/alert/{print $2;exit}' ${HOME}/.config/polybar/config)
+	#SW="%{F#${COR}}%{F-}"
+	SW="%{F#000000}%{F-}"
     if [ $1 ]; then
     	cat ${HOME}/.config/polybar/branco > ${HOME}/.config/polybar/config
 	fi
