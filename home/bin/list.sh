9f371b7ce3e19915a95f1bb4bfa873522938d697
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-01-09 05:05:01 -0300

    Autocommit por Git-Cron: alterações: add 'home/bin/list.sh'

--- home/bin/list.sh
+++ home/bin/list.sh
@@ -12,26 +12,31 @@
 # Última alteração: 08/01/2019 21:15:41
 
 [ -f ${HOME}/.config/user-dirs.dirs ] && . ${HOME}/.config/user-dirs.dirs
+
+tipo="video"
+icone="${HOME}/.local/share/icons/elementary/preferences-system-network.png"
 pasta="${XDG_DESKTOP_DIR:-${HOME}/desk}" 	# Diretório para salvar os arquivos.
-[ ! -d $pasta ] && mkdir -p $pasta
 lixeira="${HOME}/.local/share/Trash"
 url="$(xclip -o)"
-icone="${HOME}/.local/share/icons/elementary/camera-photo.png"
-# /usr/share/sounds/freedesktop/stereo/
-som="complete"
-lista="/tmp/lista.txt"
+som="complete" # /usr/share/sounds/freedesktop/stereo/
+#lista="/tmp/lista.txt"
+readarray lista < /tmp/lista.txt
+
+[ ! -d $pasta ] && mkdir -p $pasta
 
 if [ "$1" == "-a" ]; then
 	[[ ! -z "$url" ]] && echo $url >> $lista
+	DISPLAY=:0 canberra-gtk-play -i $som 2>&1
+	dbus-launch notify-send -i $icone "Batch Downloader" "Transfêrencias de $url adicionada a $lista"
 elif [ "$1" == "-d" ]; then
 	[ -f $lista ] && rm $lista
-elif [ "$1" == "-xv" ]; then
-	youtube-dl -a $lista
-	DISPLAY=:0 canberra-gtk-play -i $som 2>&1
-	dbus-launch notify-send -i $icone "Batch Downloader" "Transfêrencias finalizadas."
-elif [ "$1" == "-xa" ]; then
-	youtube-dl --extract-audio --audio-format mp3 -a $lista
-        DISPLAY=:0 canberra-gtk-play -i $som 2>&1
-	dbus-launch notify-send -i $icone "Batch Downloader" "Transfêrencias finalizadas."
+	DISPLAY=:0 canberra-gtk-play -i 'trash-empty' 2>&1
+	dbus-launch notify-send -i $icone "Batch Downloader" "/tmp/lista.txt apagada."
+elif [ "$1" == "-x" ]; then
+	for i in "${lista[@]}"; do
+		[ "$2" == "-a" ] && youtube-dl --extract-audio --audio-format mp3 $i || youtube-dl $i
+		mv $i $pasta/
+		DISPLAY=:0 canberra-gtk-play -i $som 2>&1
+		dbus-launch notify-send -i $icone "Batch Downloader" "Transfêrencias de $i finalizada."
+	done
 fi
-
