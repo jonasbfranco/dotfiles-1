fc5b63c7cda44527404eb231e914745c8e690001
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-01-09 21:00:01 -0300

    Autocommit por Git-Cron: alterações: remove 'home/bin/list.sh' add 'home/bin/dl.sh'

--- home/bin/dl.sh
+++ home/bin/dl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Arquivo: list.sh
+# Arquivo: dl.sh
 #
 # Cria uma lista.txt em /tmp
 # Baixa todos os links contidos nesta lista usando o youtube-dl
@@ -20,11 +20,17 @@ lixeira="${HOME}/.local/share/Trash"
 url="$(xclip -o)"
 som="complete" # /usr/share/sounds/freedesktop/stereo/
 #lista="/tmp/lista.txt"
-readarray lista < /tmp/lista.txt
+[ "$1" != "-dl" ] && readarray lista < /tmp/lista.txt
 
 [ ! -d $pasta ] && mkdir -p $pasta
 
-if [ "$1" == "-a" ]; then
+if [ "$1" == "-dl" ]; then
+	cd $pasta
+	dbus-launch notify-send -i $icone "Batch Downloader" "O download de $url foi iniciado."
+	[ "$2" == "-a" ] && youtube-dl --extract-audio --audio-format mp3 $url || youtube-dl $url
+	DISPLAY=:0 canberra-gtk-play -i $som 2>&1
+	dbus-launch notify-send -i $icone "Batch Downloader" "Transfêrencias de $url finalizada."
+elif [ "$1" == "-a" ]; then
 	[[ ! -z "$url" ]] && echo $url >> /tmp/lista.txt
 	DISPLAY=:0 canberra-gtk-play -i $som 2>&1
 	dbus-launch notify-send -i $icone "Batch Downloader" "$url adicionada a /tmp/lista.txt"
