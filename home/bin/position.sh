e0d1f751741c2d09d032f1f5468cbfd82dce3e1b
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-01-17 03:15:02 -0300

    Autocommit por Git-Cron: alterações: add 'home/bin/position.sh'

--- home/bin/position.sh
+++ home/bin/position.sh
@@ -7,6 +7,8 @@
 # Criado em: 16-03-2018 16:35:20
 # Última alteração: 17/01/2019 02:50:37
 
+barra=59 # Altura da barra do DE/WM
+
 IFS='x' read larguraTela alturaTela < <(xdpyinfo | grep dimensions | grep -o '[0-9x]*' | head -n1)
 
 largura=$(xdotool getactivewindow getwindowgeometry --shell | head -4 | tail -1 | sed 's/[^0-9]*//')
@@ -15,21 +17,23 @@ altura=$(xdotool getactivewindow getwindowgeometry --shell | head -5 | tail -1 |
 centroX=$((larguraTela/2-largura/2))
 centroY=$((alturaTela/2-altura/2))
 
-top=0
-bottom=$((alturaTela+altura/2))
-left=0
-right=$((larguraTela-673))
+topo=0
+baixo=$((alturaTela-altura))
+esquerda=0
+right=$((larguraTela-largura))
 
-notify-send "$right $bottom"
+notify-send "$right $baixo"
 
 if [ "$1" == "-tl" ]; then
-	xdotool getactivewindow windowmove "$right" "$left"
+	xdotool getactivewindow windowmove "$esquerda" "$topo"
 elif [ "$1" == "-tr" ]; then
-	xdotool getactivewindow windowmove "$top" "$right"
+	xdotool getactivewindow windowmove "$right" "$topo"
 elif [ "$1" == "-bl" ]; then
-	xdotool getactivewindow windowmove "$bottom" "$left"
+	baixo=$((baixo-barra))
+	xdotool getactivewindow windowmove "$esquerda" "$baixo"
 elif [ "$1" == "-br" ]; then
-	xdotool getactivewindow windowmove "$bottom" "$right"
+	baixo=$((baixo-barra))
+	xdotool getactivewindow windowmove "$right" "$baixo"
 else
 	xdotool getactivewindow windowmove "$centroX" "$centroY"
 fi
