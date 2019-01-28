34e6092bd1403ee374b4d1d445e667f35117bac8
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-01-28 01:37:01 -0300

    Autocommit por Git-Cron: alterações: add 'polybar/.config/polybar/config' add 'polybar/.config/polybar/scripts/tray.sh'

--- polybar/.config/polybar/scripts/tray.sh
+++ polybar/.config/polybar/scripts/tray.sh
@@ -4,10 +4,9 @@ string='tray-position'
 
 if grep ";$string" ${HOME}/.config/polybar/config 1> /dev/null; then
 	sed -e "/$string/ s/^;*//" -i ${HOME}/.config/polybar/config
-	echo "Removendo ;"
 else
 	sed -e "/$string/ s/^;*/;/" -i ${HOME}/.config/polybar/config
-	echo "Adicionando ;"
 fi
 
+i3-msg restart
 
