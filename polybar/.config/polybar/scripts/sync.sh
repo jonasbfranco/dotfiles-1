fe2c6a0de72696283c71b56109fc485652f26b0c
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-03-30 09:30:01 -0400

    Autocommit por Git-Cron: alterações: add 'polybar/.config/polybar/scripts/sync.sh'

--- polybar/.config/polybar/scripts/sync.sh
+++ polybar/.config/polybar/scripts/sync.sh
@@ -22,7 +22,7 @@ elif [[ "${1}" == "-u" ]]; then
 			titulo="Atualizar o site ${site}?"
 			#l=$(largura "$titulo")
 			l=$(echo $titulo | wc -c)
-	    	confirma=$(echo -e "Sim\nNão" | rofi -p "$titulo" -dmenu -bw 0 -lines 3 -separator-style none -location 0 -width $(($l-20)) -hide-scrollbar -padding 5)
+	    	confirma=$(echo -e "Sim\nNão" | rofi -p "$titulo" -dmenu -bw 0 -lines 3 -separator-style none -location 0 -width $(($l-15)) -hide-scrollbar -padding 5)
 	    	if [ "$confirma" == "Sim" ]; then
 	    		dbus-launch notify-send -i $iconeOK "WebSite Sync" "Site <b>$site</b> atualizado."
 	    	else
