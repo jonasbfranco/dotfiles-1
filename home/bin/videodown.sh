4f1e3bcdc8d19b409fceb41c35e10adb1eee20e9
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-04-30 13:09:01 -0400

    Autocommit por Git-Cron: alterações: add 'home/bin/videodown.sh'

--- home/bin/videodown.sh
+++ home/bin/videodown.sh
@@ -9,6 +9,7 @@
 #
 # https://gist.github.com/tayfie/6dad43f1a452440fba7ea1c06d1b603a
 
+aria=1
 old="$(pwd)"
 pasta="${HOME}/desk/$$"
 turl="$(xclip -o)"
@@ -22,7 +23,12 @@ else
 fi
 
 cd $pasta
-youtube-dl -q -o '%(title)s.%(ext)s' "$turl"
+if [ $aria == 1 ]; then
+    youtube-dl -q -o -o '%(title)s.%(ext)s' --external-downloader aria2c --external-downloader-args '-c -j 3 -x 3 -s 3 -k 1M' "$turl"
+else
+    youtube-dl -q -o '%(title)s.%(ext)s' "$turl"
+fi
+
 notify-send -i $icone "Video Downloader" "Transferencia $$ finalizada."
 canberra-gtk-play -i $som
 cd $old
