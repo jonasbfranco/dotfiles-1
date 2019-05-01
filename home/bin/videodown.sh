bf85462be030bbadc99cb80251907b04ea4015de
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-05-01 14:31:01 -0400

    Autocommit por Git-Cron: alterações: add 'home/bin/videodown.sh'

--- home/bin/videodown.sh
+++ home/bin/videodown.sh
@@ -13,15 +13,14 @@ dir="${HOME}/desk"
 url="$(xclip -o)"
 icone="${HOME}/.local/share/icons/elementary/video-display.png"
 som='complete'
-
-opts='-q --restrict-filenames'
+opts='-q'
 
 [ ! -f $dir ] && mkdir -p $dir
 [ $1 ] && url="$1"
 
 padrao='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 if [[ ! ${url} =~ $padrao ]]; then 
-	echo "Link inválido."
+	notify-send -i $icone "Video Downloader" "Link inválido!"
     exit
 else
 	titulo=$(curl "$url" -so - | grep -iPo '(?<=<title>)(.*)(?=</title>)')
