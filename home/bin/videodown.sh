4cedda2183b69701e81a77dbfcf3d1e07095363a
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-05-01 15:07:01 -0400

    Autocommit por Git-Cron: alterações: add 'home/bin/videodown.sh'

--- home/bin/videodown.sh
+++ home/bin/videodown.sh
@@ -30,7 +30,7 @@ fi
 cd $dir
 
 if [ $aria == 1 ]; then
-    youtube-dl $opts -o "$titulo"'.%(ext)s' --external-downloader aria2c --external-downloader-args '-m 10 -c -j 1 -x 1 -s 3 -k 5M' "${url}"
+    youtube-dl $opts -o "$titulo"'.%(ext)s' --external-downloader aria2c --external-downloader-args '-m 10 -c -j 1 -x 1 -s 2 -k 3M' "${url}"
 else
     youtube-dl $opts -o "$titulo"'.%(ext)s' "${url}"
 fi
