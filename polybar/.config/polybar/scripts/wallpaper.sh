801f2137f12f7e65707d7070c3f5febb87ee1a76
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-05-01 14:17:01 -0400

    Autocommit por Git-Cron: alterações: add 'polybar/.config/polybar/scripts/wallpaper.sh'

--- polybar/.config/polybar/scripts/wallpaper.sh
+++ polybar/.config/polybar/scripts/wallpaper.sh
@@ -12,7 +12,7 @@
 dir="${XDG_PICTURES_DIR:-${HOME}/img/wallpapers/linux}"
 unsplash_dir="${XDG_PICTURES_DIR:-${HOME}/img}/unsplash"
 default="$dir/wallpapers/default.jpg"
-ultima="/home/lucas/img/unsplash/unsplash-22336.jpg"
+ultima="/home/lucas/img/unsplash/unsplash-26135.jpg"
 modo="--bg-fill"
 indice=0
 i=0
