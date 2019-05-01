23b6511d357fbb7a3f19bb6ffc94af3bd9a38130
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-05-01 15:00:01 -0400

    Autocommit por Git-Cron: alterações: add 'home/bin/videodown.sh'

--- home/bin/videodown.sh
+++ home/bin/videodown.sh
@@ -8,7 +8,7 @@
 # Última alteração: 01/05/2019 14:32:25
 
 aria=1
-old="$(pwd)"
+ts=$(date +"%s")
 dir="${HOME}/desk"
 url="$(xclip -o)"
 icone="${HOME}/.local/share/icons/elementary/video-display.png"
@@ -35,6 +35,14 @@ else
     youtube-dl $opts -o "$titulo"'.%(ext)s' "${url}"
 fi
 
-find . -type f -name "${titulo}*" -not -name '*mp4' -not -name '*avi' | xargs rm
+arquivos=$(ls ${titulo}* | egrep -vi ".mp4|.avi|.mkv")
+for i in "${arquivos[@]}"
+do
+    mod=$(stat -c "%Y" $i)
+    if [[ $mod > $ts ]]; then
+        rm -f "$i"
+    fi
+done
+
 notify-send -i $icone "Video Downloader" "Transferencia de <b>$titulo</b> finalizada."
 canberra-gtk-play -i $som
