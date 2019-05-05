33d145ef1a3c8f5012abc3577fb2734eaeeddca2
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-05-05 17:49:01 -0400

    Autocommit por Git-Cron: alterações: add 'home/bin/videodown.sh'

--- home/bin/videodown.sh
+++ home/bin/videodown.sh
@@ -9,13 +9,18 @@
 
 log=1 # 0 = Sem log, 1 = Log na tela, 2 = Log no arquivo erro.log
 aria=1
+retries=10 # 0 = inf
+md=1 # Max Downloads
+mc_server=5 # Max conn per server
+mc=5 # Max conn
+piece="1M" # Slit filesize
 ts=$(date +"%s")
 dir="${HOME}/desk"
 url="$(xclip -o)"
 icone="${HOME}/.local/share/icons/elementary/video-display.png"
 som='complete'
 #opts='-q'
-ariaopts='-m 10 -c -j 1 -x 3 -s 3 -k 2M'
+ariaopts="-m $retries -c -j $md -x $mc_server -s $mc -k $piece"
 
 [ ! -f $dir ] && mkdir -p $dir
 [ $1 ] && url="$1"
@@ -31,6 +36,13 @@ fi
 cd $dir
 
 if [ $aria == 1 ]; then
+    # -j, --max-concurrent-downloads
+    # -x, --max-connection-per-server
+    # -m, --max-tries
+    # -k, --min-split-size
+    # -s, --split restricted by --max-connection-per-server 
+    # -t, --timeout
+
     youtube-dl $opts -o "${titulo}.%(ext)s" --external-downloader aria2c --external-downloader-args $ariaopts "${url}" && status=$?
 else
     youtube-dl $opts -o "${titulo}.%(ext)s" "${url}" && status=$?
