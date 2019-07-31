8c41a75323b2cae8dff97d57cae6aa8a45e0bfce
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-07-30 21:39:01 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/gradientes.sh'

--- home/bin/gradientes.sh
+++ home/bin/gradientes.sh
@@ -7,34 +7,20 @@
 # Criado em: 31/10/2018 11:46:24
 # Última alteração: 17/07/2019 15:54:12
 
-if [ "$1" ]; then
-	gradientes=(
-		'#c79081 #dfa579'
-		'#bdc3c7 #2c3e50'
-		'#0F2027 #2C5364'
-		'#373B44 #4286f4'
-		'#0f0c29 #302b63'
-		'#bdc3c7 #2c3e50'
-	)
+function randColor () {
+	RANDC=$(printf "%02x%02x%02x\n" $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
+	echo "#"$RANDC
+}
 
-	gradiente=${gradientes[$RANDOM % ${#gradientes[@]}]}
-	primeiro=$(echo $gradiente | awk '{print $1;}')
-	segundo=$(echo $gradiente | awk '{print $NF}')
+cor1=$(randColor)
+cor2=$(randColor)
 
-	hsetroot -add "$primeiro" -add "$segundo" -gradient 0
+if [ "$1" == "-r" ] && [ -f $HOME/.randwall ]; then
+	cor1=$(awk 'NR==1' $HOME/.randwall)
+	cor2=$(awk 'NR==2' $HOME/.randwall)
 else
-	function randColor () {
-		RANDC=$(printf "%02x%02x%02x\n" $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
-		#RANDC="${RANDC^^}"
-		echo "#"$RANDC
-		#echo $RANDC
-		#echo $RANDC
-	}
-
-	cor1=$(randColor)
-	cor2=$(randColor)
-
-	echo $cor1
-	echo $cor2
-	hsetroot -add "$cor1" -add "$cor2" -gradient 0
+	echo "$cor1" > $HOME/.randwall
+	echo "$cor2" >> $HOME/.randwall
 fi
+
+hsetroot -add "$cor1" -add "$cor2" -gradient 0
