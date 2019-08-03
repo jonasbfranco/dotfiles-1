7f0c0905c4b7cbcf9c00658d0d7868ef59d764fc
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-08-03 16:45:01 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/tv.sh'

--- polybar/.config/polybar/scripts/tv.sh
+++ polybar/.config/polybar/scripts/tv.sh
@@ -3,7 +3,7 @@
 if pgrep -x "xautolock" > /dev/null
 then
 	[ $1 ] && killall xautolock &
-	echo ""
+	echo "%{F#D08770}%{F-}"
 else
 	[ $1 ] && xautolock -time 3 -detectsleep -locker $HOME/.local/lock/fortune &
 	echo ""
