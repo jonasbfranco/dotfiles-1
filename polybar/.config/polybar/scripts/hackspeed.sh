e3ba0ac26b21e3cbad9844899434129610968a2e
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-08-03 18:35:01 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/hackspeed.sh'

--- polybar/.config/polybar/scripts/hackspeed.sh
+++ polybar/.config/polybar/scripts/hackspeed.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2016,SC2059
 
 #KEYBOARD_ID="AT Translated Set 2 keyboard"
-KEYBOARD_ID="5"
+KEYBOARD_ID="Logitech USB Keyboard"
 
 # cpm: characters per minute
 # wpm: words per minute (1 word = 5 characters)
