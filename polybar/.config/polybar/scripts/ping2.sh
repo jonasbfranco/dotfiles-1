2f73e3f58f416be36fcb0c0f6865c1805f1ba86c
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-08-03 18:56:01 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/ping2.sh'

--- polybar/.config/polybar/scripts/ping2.sh
+++ polybar/.config/polybar/scripts/ping2.sh
@@ -7,12 +7,13 @@ if ! ping=$(ping -n -c 1 -W 1 $HOST); then
 else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
-    if [ "$rtt" -lt 50 ]; then
-        icon="%{F#A3BE8C}%{F-}"
-    elif [ "$rtt" -lt 150 ]; then
-        icon="%{F#EBCB8B}%{F-}"
-    else
+    if [ "$rtt" -lt 90 ]; then
+        #icon="%{F#A3BE8C}%{F-}"
         icon="%{F#D08770}%{F-}"
+    elif [ "$rtt" -lt 150 ]; then
+        icon="%{F#A3BE8C}%{F-}"
+    else
+        icon="%{F#EBCB8B}%{F-}"
     fi
 
     echo "$icon $rtt ms"
