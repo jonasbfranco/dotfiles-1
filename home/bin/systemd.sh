ebfba352a24d11ab74ec2141165e1bd924f275f0
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-08-05 11:30:02 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/systemd.sh'

--- home/bin/systemd.sh
+++ home/bin/systemd.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-if systemctl is-active --quiet $1; then
-    echo OK
+if [ $1 ]; then
+    if systemctl is-active --quiet $1; then
+        if [ $2 ]; then
+            systemctl stop $1
+        fi
+        echo "$1 ativo"
+    else
+        if [ $2 ]; then
+            systemctl start $1
+        fi
+        echo "$1 parado"
+    fi
 fi
