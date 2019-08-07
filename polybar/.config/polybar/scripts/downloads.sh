cbd161442bd30939430393d0978d472b2a64445a
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-08-07 14:18:01 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/config.nord' add 'polybar/.config/polybar/scripts/downloads.sh'

--- polybar/.config/polybar/scripts/downloads.sh
+++ polybar/.config/polybar/scripts/downloads.sh
@@ -2,8 +2,17 @@
 
 downloads=$(ps -A | grep youtube-dl | wc -l)
 
-if [ $downloads -gt 0 ]; then
+if [ $1 ]; then
+
+	$HOME/bin/videodown.sh
 	echo "%{F#D08770}%{F-} $downloads"
+
 else
-	echo "%{F#D08770}%{F-}"
+
+
+	if [ $downloads -gt 0 ]; then
+		echo "%{F#D08770}%{F-} $downloads"
+	else
+		echo "%{F#D08770}%{F-}"
+	fi
 fi
