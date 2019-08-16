b03f67265ec4901a4e0a519c934c2e65bf4ec0f5
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-08-16 04:33:02 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/videodown.sh'

--- home/bin/videodown.sh
+++ home/bin/videodown.sh
@@ -12,8 +12,8 @@
 log=0 # 0 = Sem log, 1 = Log no arquivo erro.log
 aria=0
 ts=$(date +"%s")
-#dir="${HOME}/desk"
-dir="${HOME}/desk/$(ls -t -1 ${HOME}/desk | head -1)"
+dir="${HOME}/desk"
+#dir="${HOME}/desk/$(ls -t -1 ${HOME}/desk | head -1)"
 icone="${HOME}/.local/share/icons/elementary/video-display.png"
 som='complete'
 tmp="/tmp/videodown/$$"
