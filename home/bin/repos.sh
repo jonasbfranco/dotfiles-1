82706c4712dc3710e22d2a50a47edcc85268bb53
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-11-24 20:41:01 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/repos.sh'

---
+++ home/bin/repos.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Source: https://stackoverflow.com/a/32833411
+
+[ $# -lt 2 ] && echo "Número incorreto de parâmetros" && exit
+
+if [ $# -lt 3 ]; then
+    pagina=1
+else
+    pagina=$3
+fi
+
+if [ "$1" == "github" ]; then
+    # Github
+    curl "https://api.github.com/users/$NAME/repos?page=$pagina&per_page=100" |
+        grep -e 'git_url*' |
+        cut -d \" -f 4
+else
+    # Gitlab
+    # ID: https://gitlab.com/api/v4/users?username=[apelido]
+    # Projetos: https://gitlab.com/api/v4/users/[id]/projects
+    #TOKEN="PASTE_YOUR_PRIVATE_TOKEN_HERE";
+
+    # ssh_url_to_repo / http_url_to_repo
+    PREFIX="ssh_url_to_repo";
+    #curl --header "PRIVATE-TOKEN: $TOKEN" http://gitlab.com/api/v3/projects | grep -o "\"$PREFIX\":[^ ,]\+" | xargs -L1 basename | awk -F '.' '{print $1}'
+    curl http://gitlab.com/api/v3/projects | grep -o "\"$PREFIX\":[^ ,]\+" | xargs -L1 basename | awk -F '.' '{print $1}'
+fi
