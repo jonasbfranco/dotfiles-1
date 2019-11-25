8f301b45b20c044067c0ddfcce4ad7d0c4df4c3d
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-11-25 18:55:02 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/tv.sh'

--- polybar/.config/polybar/scripts/tv.sh
+++ polybar/.config/polybar/scripts/tv.sh
@@ -2,9 +2,9 @@
 
 if pgrep -x "xautolock" > /dev/null
 then
-	[ $1 ] && killall xautolock && notify-send -i ~/.local/share/icons/elementary/screensaver.png "Screen Locker" "Proteção de tela desativada." &
+	[ $1 ] && killall xautolock ; notify-send -i ~/.local/share/icons/elementary/screensaver.png "Screen Locker" "Proteção de tela desativada." &
 	echo "%{F#D08770}%{F-}"
 else
-	[ $1 ] && xautolock -time 3 -detectsleep -locker $HOME/.local/lock/fortune && notify-send -i ~/.local/share/icons/elementary/screensaver.png "Screen Locker" "Proteção de tela ativada." &
+	[ $1 ] && xautolock -time 3 -detectsleep -locker $HOME/.local/lock/fortune ; notify-send -i ~/.local/share/icons/elementary/screensaver.png "Screen Locker" "Proteção de tela ativada." &
 	echo ""
 fi
