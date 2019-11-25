507be37648a94138c6424f35c15dffa20422496e
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-11-25 14:04:01 -0400

    Autocommit por GitHub.sh: alterações: add 'home/.local/share/dbus-1/services/org.freedesktop.Notifications.service' add 'polybar/.config/polybar/scripts/wallpaper.sh'

--- polybar/.config/polybar/scripts/wallpaper.sh
+++ polybar/.config/polybar/scripts/wallpaper.sh
@@ -12,7 +12,7 @@
 dir="${XDG_PICTURES_DIR:-${HOME}/img/wallpapers/linux}"
 unsplash_dir="${XDG_PICTURES_DIR:-${HOME}/img}/unsplash"
 default="$dir/wallpapers/default.jpg"
-ultima="/home/lucas/img/unsplash/unsplash-22198.jpg"
+ultima="/home/lucas/img/porn/ts/Paula Melo/10781/40745_11.jpg"
 modo="--bg-fill"
 indice=0
 i=0
