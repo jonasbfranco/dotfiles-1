d930d13a1e3e3bccafd995434fb86e6187c3382f
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-11-25 18:34:01 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/tv.sh'

--- polybar/.config/polybar/scripts/tv.sh
+++ polybar/.config/polybar/scripts/tv.sh
@@ -5,6 +5,6 @@ then
 	[ $1 ] && killall xautolock &
 	echo "%{F#D08770}%{F-}"
 else
-	[ $1 ] && xautolock -time 3 -detectsleep -locker $HOME/.local/lock/fortune &
+	[ $1 ] && xautolock -time 3 -detectsleep -locker $HOME/.local/lock/fortune && notify-send -i ~/.config/dunst/icons/moon.svg "Screen Locker" "$(python2 ~/.config/polybar/scripts/moonphase.py desc)" &
 	echo ""
 fi
