aa75cf60bd40148f6c146fc1bedc25872616c172
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-11-28 23:30:02 -0400

    Autocommit por GitHub.sh: alterações: add 'docker/docker/scripts/dockerctl.sh' add 'home/bin/dockerctl.sh'

--- home/bin/dockerctl.sh
+++ home/bin/dockerctl.sh
@@ -14,18 +14,34 @@ if [ "$1" == "start" ]; then
 fi
 
 if [ "$1" == "rm" ]; then
-    docker ps -q | xargs docker stop
-    docker ps -a -q | xargs docker rmi -v
+    read -p "Tem certeza que deseja remover TODOS os containers?" resp
+    if [[ "$resp" == [sS]* ]]; then
+        if [[ "$(docker ps -q -a)" ]]; then
+            docker ps -q | xargs docker stop
+            docker ps -a -q | xargs docker rm -v
+        fi
+    fi
 fi
 
 if [ "$1" == "rmi" ]; then
-    docker ps -q | xargs docker stop
-    docker ps -a -q | xargs docker rmi -v
-    docker images -q | xargs docker rmi -v
+    read -p "Tem certeza que deseja remover TODAS as imagens?" resp
+    if [[ "$resp" == [sS]* ]]; then
+        docker ps -q | xargs docker stop
+        docker ps -a -q | xargs docker rmi -v
+        docker images -q | xargs docker rmi
+    fi
 fi
 
 if [ "$1" == "upgrade" ]; then
     docker ps -a -q | xargs docker stop
     docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull 
     docker ps -a -q | xargs docker start
+fi
+
+if [ "$1" == "rebuild" ]; then
+    if [ -f docker-compose.yml ]; then
+        docker-compose up -d --build
+    else
+        echo "Não existe um arquivo docker-compose.yml no diretório atual."
+    fi
 fi
