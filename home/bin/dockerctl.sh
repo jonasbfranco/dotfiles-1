bd1bb8da8629fdc45ac68890207891515f9b3371
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-11-28 23:00:02 -0400

    Autocommit por GitHub.sh: alterações: add 'docker/docker/scripts/dockerctl.sh' add 'home/bin/dockerctl.sh'

--- home/bin/dockerctl.sh
+++ home/bin/dockerctl.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# 
+# dockerctl.sh
+#
+# Criado em: 28-11-2019 23:50:39
+# Alterado em: 28-11-2019 23:50:45
 
 if [ "$1" == "stop" ]; then
 	docker ps -q | xargs docker stop
@@ -8,10 +13,19 @@ if [ "$1" == "start" ]; then
 	docker ps -q | xargs docker stop
 fi
 
+if [ "$1" == "rm" ]; then
+    docker ps -q | xargs docker stop
+    docker ps -a -q | xargs docker rmi -v
+fi
+
 if [ "$1" == "rmi" ]; then
+    docker ps -q | xargs docker stop
+    docker ps -a -q | xargs docker rmi -v
     docker images -q | xargs docker rmi -v
 fi
 
 if [ "$1" == "upgrade" ]; then
+    docker ps -a -q | xargs docker stop
     docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull 
+    docker ps -a -q | xargs docker start
 fi
