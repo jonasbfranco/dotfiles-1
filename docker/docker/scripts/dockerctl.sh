60792a196b88d23ec02280093c07bfeadef36fba
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-11-28 22:39:55 -0400

    Alterações

---
+++ docker/docker/scripts/dockerctl.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "stop" ]; then
+	docker ps -q | xargs docker stop
+fi
+
+if [ "$1" == "stop" ]; then
+    docker ps -q | xargs docker stop
+fi
+
+if [ "$1" == "stop" ]; then
+    docker ps -q | xargs docker stop
+fi
+
+if [ "$1" == "stop" ]; then
+    docker ps -q | xargs docker stop
+fi
+
