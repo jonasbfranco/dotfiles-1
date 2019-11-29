a38eb471af1d385aaf7205160c22a76607f5ccb8
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-11-29 00:45:01 -0400

    Autocommit por GitHub.sh: alterações: add 'etc/hosts.d/01-default.conf' add 'etc/hosts.d/02-hblock.conf' add 'etc/hosts.d/hblock.md5' remove 'etc/hosts.d/hblock.tmp' add 'etc/hosts.d/init.sh'

--- etc/hosts.d/init.sh
+++ etc/hosts.d/init.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+[ "$EUID" -ne 0 ] && echo "É necessário rodar o script como root." && exit 1
+
 url="https://hblock.molinero.dev/hosts"
 curl -s -L "$url" > hblock.tmp
+sed -i '0,/^# <\/header>$/d' hblock.tmp
 
-if md5sum -c hblock.md5; then
-	rm hblock.tpm
+if md5sum -c hblock.md5 > /dev/null; then
+	rm hblock.tmp
 else
     mv hblock.md5 02-hblock.conf
     [ -f /etc/hosts ] && mv /etc/hosts /etc/hosts.d/hosts.bkp.$(date +'%s')
