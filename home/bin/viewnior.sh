a26a881e7ba794916d0167d65fcb919d4387a22d
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-04 23:43:11 -0400

    Autocommit por GitHub.sh: alterações: add 'home/.local/bin/screencast-xfce' add 'home/bin/viewnior.sh' add 'home/.local/share/icons/elementary/user-available.png' add 'home/.local/share/icons/elementary/user-available.svg' add 'home/.local/share/icons/elementary/user-away.svg' add 'home/.local/share/icons/elementary/user-busy.svg' add 'home/.local/share/icons/elementary/user-offline.png' add 'home/.local/share/icons/elementary/user-offline.svg'

--- home/bin/viewnior.sh
+++ home/bin/viewnior.sh
@@ -6,7 +6,7 @@
 # Autor: Lucas Saliés Brum a.k.a. sistematico <lucas@archlinux.com.br>         #
 #                                                                              #
 # Criado em: 16-03-2018 16:35:20 pm                                             #
-# Modificado em: 05-12-2019 12:29:55 am                                        #
+# Modificado em: 05-12-2019 12:31:19 am                                        #
 #                                                                              #
 # Este trabalho está licenciado com uma Licença Creative Commons               #
 # Atribuição 4.0 Internacional                                                 #
@@ -18,10 +18,10 @@ shopt -s globstar dotglob nocaseglob
 
 if [ $1 ]; then
     if [ $2 ]; then
-        DISPLAY=:0 viewnior $1/**/*${2}*.{jpg,png}
+        DISPLAY=:0 viewnior $1/**/*${2}*.{jpg,png,svg}
     else
-        DISPLAY=:0 viewnior $1/**/*.{jpg,png}
+        DISPLAY=:0 viewnior $1/**/*.{jpg,png,svg}
     fi
 else
-    DISPLAY=:0 viewnior $(pwd)/**/*.{jpg,png}
+    DISPLAY=:0 viewnior **/*.{jpg,png,svg}
 fi
