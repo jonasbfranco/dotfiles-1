234d7ccae09912462638d64e1b689d47b08ff705
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-05 03:05:32 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/videosound.sh'

--- home/bin/videosound.sh
+++ home/bin/videosound.sh
@@ -6,7 +6,7 @@
 # Autor: Lucas Saliés Brum a.k.a. sistematico <lucas@archlinux.com.br>         #
 #                                                                              #
 # Criado em: 05-12-2019 3:11:07 am                                             #
-# Modificado em: 05-12-2019 3:58:46 am                                         #
+# Modificado em: 05-12-2019 4:05:23 am                                         #
 #                                                                              #
 # Este trabalho está licenciado com uma Licença Creative Commons               #
 # Atribuição 4.0 Internacional                                                 #
@@ -25,7 +25,7 @@ if [ $2 ]; then
         exit 1
     fi
 
-    ffmpeg -i ${1} -i ${2} -c:v copy -map 0:v:0 -map 1:a:0 new.mp4
+    ffmpeg -i ${1} -i ${2} -c:v copy -map 0:v:0 -map 1:a:0 ${1%%.*}.new.${1#*.}
 else
     echo "Uso: $(basename $0) video.mp3 audio.mp3"
 fi
