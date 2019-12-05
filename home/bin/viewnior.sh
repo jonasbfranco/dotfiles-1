139a670fca294a79213c792dc02a0934d4ab0837
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-04 23:30:01 -0400

    Autocommit por GitHub.sh: alterações: add 'home/.local/bin/screencast' add 'home/.local/bin/screencast-xfce' add 'home/bin/viewnior.sh'

--- home/bin/viewnior.sh
+++ home/bin/viewnior.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
-#
-# Arquivo: viewnior.sh
-#
-# Feito por Lucas Saliés Brum a.k.a. sistematico, <lucas@archlinux.com.br>
-#
-# Criado em: 16/03/2018 16:35:20
-# Última alteração: 29-11-2019 14:50:28
+################################################################################
+#                                                                              #
+# viewnior.sh                                                                  #
+#                                                                              #
+# Autor: Lucas Saliés Brum a.k.a. sistematico <lucas@archlinux.com.br>         #
+#                                                                              #
+# Criado em: 16-03-2018 16:35:20 pm                                             #
+# Modificado em: 05-12-2019 12:29:55 am                                        #
+#                                                                              #
+# Este trabalho está licenciado com uma Licença Creative Commons               #
+# Atribuição 4.0 Internacional                                                 #
+# http://creativecommons.org/licenses/by/4.0/                                  #
+#                                                                              #
+################################################################################
 
 shopt -s globstar dotglob nocaseglob
 
 if [ $1 ]; then
-    DISPLAY=:0 viewnior $1/**/*.jpg
+    if [ $2 ]; then
+        DISPLAY=:0 viewnior $1/**/*${2}*.{jpg,png}
+    else
+        DISPLAY=:0 viewnior $1/**/*.{jpg,png}
+    fi
 else
-    DISPLAY=:0 viewnior **/*.jpg
+    DISPLAY=:0 viewnior $(pwd)/**/*.{jpg,png}
 fi
