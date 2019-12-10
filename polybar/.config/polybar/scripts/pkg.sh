3edf30bbc1aa0490ba5c4d431c1ec6be2e67e499
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-10 17:30:01 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/pkg.sh' add 'home/.local/share/icons/elementary/package.svg'

--- polybar/.config/polybar/scripts/pkg.sh
+++ polybar/.config/polybar/scripts/pkg.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
 pac=$(checkupdates 2> /dev/null | wc -l)
 aur=$(yay -Qum 2> /dev/null | wc -l)
+icone="${HOME}/.local/share/icons/elementary/package.svg"
 
-#echo "$pac %{F#5b5b5b}%{F-} $aur"
-echo "${pac}/${aur}"
+if [ $1 ]; then
+	if [ $pac -gt 0 ] || [ $aur -gt 0 ]; then
+		$TERMINAL -e "yay -Syu"
+	else
+		notify-send -i $icone "Updates" "Nenhuma atualização disponível."
+	fi
+else
+	echo "${pac}/${aur}"
+fi
