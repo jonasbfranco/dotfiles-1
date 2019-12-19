62c89a521d15f57362f6ba5cb141bf2a9c7d1124
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-18 22:00:01 -0400

    Autocommit por GitHub.sh: alterações: add 'home/.local/bin/mkchromecast.sh'

--- home/.local/bin/mkchromecast.sh
+++ home/.local/bin/mkchromecast.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-arquivo="$1"
-base=$(echo "${arquivo%.*}")
-legenda="${base}.srt"
-params="--video -i"
+pgrep -x mkchromecast > /dev/null && sudo pkill -9 mkchromecast
+#pgrep -x mpv >/dev/null && sudo pkill -9 mpv
 
-[ -f "$legenda" ] && sub="--subtitles $legenda"
-[ -f "$arquivo" ] && mkchromecast "$params" "$arquivo" "$sub"
+sleep 1
+
+arquivo=${1}
+base=$(echo ${arquivo%.*})
+legenda="${base}.srt"
+params="-i"
+
+[ -f "$legenda" ] && sub="--subtitles ${legenda}"
+#[ -f "$arquivo" ] && mkchromecast $params "$arquivo" $sub > /dev/null 2> /dev/null &
+[ -f "$arquivo" ] && /usr/bin/mkchromecast "$params" \"${arquivo}\" \"${sub}\"
