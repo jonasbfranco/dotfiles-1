d49ce8151df73dfff063abcaddb225eb8323ff4e
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-24 12:45:01 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/sound-theme.sh'

--- home/bin/sound-theme.sh
+++ home/bin/sound-theme.sh
@@ -8,4 +8,15 @@ fi
 
 temas=($(ls /usr/share/sounds))
 
-xfconf-query -c xsettings -p /Net/SoundThemeName -s Yaru
+tema=$(yad --width 300 --entry --title "Tema de Som" \
+    #--image=gnome-shutdown                        \
+    --image=gtk-play                    \
+    --button="gtk-ok:0" --button="gtk-close:1"    \
+    --text "Choose action:"                       \
+    --entry-text                                  \
+    $(ls /usr/share/sounds))
+
+if [ $tema ]; then
+	#xfconf-query -c xsettings -p /Net/SoundThemeName -s Yaru
+	echo $tema
+fi
