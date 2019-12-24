8a2e978c251cb7a1e7b442c11a9c73529e297027
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-24 18:30:01 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/config.nord' add 'polybar/.config/polybar/scripts/btc.sh' add 'polybar/.config/polybar/scripts/dollar.sh' remove 'polybar/.config/polybar/scripts/ticker-crypto.sh' add 'polybar/.config/polybar/scripts/stream.sh' add 'polybar/.config/polybar/scripts/usd.sh'

--- polybar/.config/polybar/scripts/ticker-crypto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-api="https://www.mercadobitcoin.net/api/BTC/ticker/"
-quote=$(curl -sf $api | jq -r ".ticker.last")
-quote=$(LANG=C printf "%'.4s" ${quote})
-quote=$(LANG=pt_BR.UTF-8 printf "%'.2f" ${quote})
-echo " $quote"
--- polybar/.config/polybar/scripts/usd.sh
+++ polybar/.config/polybar/scripts/usd.sh
@@ -7,8 +7,9 @@ else
 	exit
 fi
 
-api=$(curl -s -H "X-CMC_PRO_API_KEY: ${coinmarketcap}" -H "Accept: application/json" -d "id=1&amount=1&convert=BRL" -G https://pro-api.coinmarketcap.com/v1/tools/price-conversion)
+# BTC 1, USD 2781, BRL 2783
+api=$(curl -s -H "X-CMC_PRO_API_KEY: ${coinmarketcap}" -H "Accept: application/json" -d "id=2781&amount=1&convert=BRL" -G https://pro-api.coinmarketcap.com/v1/tools/price-conversion)
 quote=$(echo $api | jq -r ".data.quote.BRL.price")
 quote=$(LANG=C printf "%'.2f" "${quote}")
 
-echo "BTC $quote"
+echo "USD $quote"
