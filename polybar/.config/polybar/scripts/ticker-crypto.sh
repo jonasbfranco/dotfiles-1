f04706bc5f0af0529cb87d0941d289d8d0af42c8
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-24 17:30:01 -0400

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/ticker-crypto.sh'

--- polybar/.config/polybar/scripts/ticker-crypto.sh
+++ polybar/.config/polybar/scripts/ticker-crypto.sh
@@ -2,6 +2,8 @@
 
 api="https://www.mercadobitcoin.net/api/BTC/ticker/"
 quote=$(curl -sf $api | jq -r ".ticker.last")
-quote=$(LANG=C printf "%.2f" "$quote")
+#quote=$(LANG=C printf "%.2f" "$quote")
+#quote=$(LANG=C printf "%.2e" "$quote")
+quote=$(LC_NUMERIC="pt_BR.UTF-8" printf "%'.2f" "$quote")
 
 echo " $quote"
