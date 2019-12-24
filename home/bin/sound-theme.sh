f33ca9a596c8b6b1054272d4be694dfdc9ef4b5f
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-24 13:15:01 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/sound-theme.sh' add 'home/bin/var.sh'

--- home/bin/sound-theme.sh
+++ home/bin/sound-theme.sh
@@ -6,6 +6,19 @@ if [ $? = 1 ]; then
     exit
 fi
 
+gtk3="$HOME/.config/gtk-3.0/settings.ini"
+gtk2="$HOME/.gtkrc-2.0"
+
+toggle() {
+    if [ -f $1 ]; then
+		pattern="$2"
+		value="$3"
+        if grep -q "$pattern" "$1"; then
+			sed -i "s|${pattern}=.*|${pattern}=$value|g" $1
+        fi
+    fi
+}
+
 tema=$(yad --width 300 --entry --title "Tema de Som" \
     --image=gnome-shutdown                        \
     --button="gtk-ok:0" --button="gtk-close:1"    \
@@ -14,14 +27,19 @@ tema=$(yad --width 300 --entry --title "Tema de Som" \
     $(ls /usr/share/sounds))
 
 if [ $tema ]; then
-	if [ -f $HOME/.gtkrc-2.0 ]; then
-		sed -i 's/Var2=.*/Var2=smurf/' file.txt
+	if [ -f $gtk2 ]; then
+		if grep -q 'gtk-sound-theme-name' "$gtk2"; then
+			sed -i 's/Var2=.*/Var2=smurf/' $gtk2
+		fi
 	fi
 
-    if [ -f $HOME/.gtkrc-2.0 ]; then
-        sed -i 's/Var2=.*/Var2=smurf/' file.txt
+    if [ -f $HOME/.config/gtk-3.0/settings.ini ]; then
+		if grep -q 'gtk-sound-theme-name' "$gtk3"; then
+        	#sed -i 's/gtk-sound-theme-name=.*/gtk-sound-theme-name=smurf/' $gtk3
+			sed -i "s|$var|r_str|g"
+		fi
     fi
 
-	xfconf-query -c xsettings -p /Net/SoundThemeName -s $tema
-	DISPLAY=:0 canberra-gtk-play -i complete
+	#xfconf-query -c xsettings -p /Net/SoundThemeName -s $tema
+	#DISPLAY=:0 canberra-gtk-play -i complete
 fi
