fc5170257dce9d710f71102f9f41047bba9c7121
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-12-24 13:00:01 -0400

    Autocommit por GitHub.sh: alterações: add 'gtk/.config/gtk-3.0/gtk.css' remove 'home/.config/gtk-3.0/gtk.css' remove 'home/.config/gtk-3.0/settings.ini' add 'home/bin/sound-theme.sh' add 'gtk/.config/gtk-3.0/settings.ini' add 'gtk/.gtkrc-2.0'

--- home/bin/sound-theme.sh
+++ home/bin/sound-theme.sh
@@ -6,17 +6,22 @@ if [ $? = 1 ]; then
     exit
 fi
 
-temas=($(ls /usr/share/sounds))
-
 tema=$(yad --width 300 --entry --title "Tema de Som" \
-    #--image=gnome-shutdown                        \
-    --image=gtk-play                    \
+    --image=gnome-shutdown                        \
     --button="gtk-ok:0" --button="gtk-close:1"    \
     --text "Choose action:"                       \
     --entry-text                                  \
     $(ls /usr/share/sounds))
 
 if [ $tema ]; then
-	#xfconf-query -c xsettings -p /Net/SoundThemeName -s Yaru
-	echo $tema
+	if [ -f $HOME/.gtkrc-2.0 ]; then
+		sed -i 's/Var2=.*/Var2=smurf/' file.txt
+	fi
+
+    if [ -f $HOME/.gtkrc-2.0 ]; then
+        sed -i 's/Var2=.*/Var2=smurf/' file.txt
+    fi
+
+	xfconf-query -c xsettings -p /Net/SoundThemeName -s $tema
+	DISPLAY=:0 canberra-gtk-play -i complete
 fi
