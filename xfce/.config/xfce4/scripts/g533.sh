85731450021e47a556a0e5a41e62cd2aeb407477
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-01-19 15:15:02 -0400

    Autocommit por GitHub.sh: alterações: add 'home/.local/share/icons/panel/batt/100.png' add 'home/.local/share/icons/panel/batt/25.png' add 'home/.local/share/icons/panel/batt/5.png' add 'home/.local/share/icons/panel/batt/50.png' add 'home/.local/share/icons/panel/batt/75.png' add 'xfce/.config/xfce4/scripts/g533.sh' add 'home/.local/share/icons/panel/batt/charging.png'

--- xfce/.config/xfce4/scripts/g533.sh
+++ xfce/.config/xfce4/scripts/g533.sh
@@ -11,7 +11,7 @@ batt=$(sudo /usr/local/bin/headsetcontrol -b | grep Battery | awk '{print $2}')
 batt=${batt%?}
 
 if [ "$batt" == "Chargin" ]; then
-	echo "<img>${icon}/full.png</img>"
+	echo "<img>${icon}/charging.png</img>"
 elif [ "$batt" -ge 90 ]; then
     echo "<img>${icon}/100.png</img>"
 elif [ "$batt" -ge 75 ]; then
